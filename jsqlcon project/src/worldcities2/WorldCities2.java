428201041a13a5b4bd2c72aa2b73ec05b5f3ad6b
Numan Çiftçi <128406291+NumaIYI@users.noreply.github.com>
2023-08-26 19:52:56 +0300

    WorldCities2.java

--- jsqlcon project/src/worldcities2/WorldCities2.java
+++ jsqlcon project/src/worldcities2/WorldCities2.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package worldcities2;
 
-/**
- *
- * @author numan
- */
 public class WorldCities2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
